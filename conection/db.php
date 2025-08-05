279291e0cee76beb066cf366168feb74078a19bc
yasirkun216 <yasir.mubarok_ti21@nusaputra.ac.id>
2025-08-05 15:59:37 +0700

    data cianjur

--- conection/db.php
+++ conection/db.php
@@ -1,10 +1,13 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "evakuasi";
+$host = 'localhost';
+$user = 'root';
+$pass = ''; // kosong jika default XAMPP
+$dbname = 'evakuasi_db';
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+// Cek koneksi
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
+?>
